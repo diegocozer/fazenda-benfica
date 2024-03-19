56fad13c996d932e2c1681dd9a403702198c744e
Diego Cozer <diego.cozer16@gmail.com>
2024-03-18 23:42:32 -0300

    Removido opção do navbar

--- src/components/Fotos.jsx
+++ src/components/Fotos.jsx
@@ -6,13 +6,13 @@ import mesa from '../image/mesa-jantar.jpg';
 
 function Fotos() {
   return (
-    <div className="mt-10 px-10  " id="sobre-nos">
+    <div className="mt-10 px-10 " id="sobre-nos">
       <div className="container mx-auto px-4" />
       <div>
         <h3 className="text-2xl text-center text-gray-500 font-semibold mb-8 text-gray-400 font-bold" data-aos="zoom-in">
           Conheça mais a fazenda
         </h3>
-        <div className="grid grid-cols-4 grid-rows-2 gap-6">
+        <div className="grid grid-cols-4 grid-rows-2 gap-2 md:gap-6">
           <div className="relative col-span-2 row-span-2 rounded-lg overflow-hidden" data-aos="zoom-in">
             <div className="gradient-container absolute bottom-0 pl-4 pb-4">
               <p className=" text-white font-bold -mb-3">
